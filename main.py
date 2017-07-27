6f7f72d0821a6dc7257e4331cc5d7462ef79ceca
biya17 <biya.desalegne@gmail.com>
2017-07-27 14:20:11 -0700

    Add files via upload
    Commentbox

--- main.py
+++ main.py
@@ -1,67 +1,75 @@
-import webapp2
-import sys
 import jinja2
 import os
+import webapp2
+import sys
 from random import randint
 
 from os import path
 sys.path.append('source/')
 
-from webpost import BlogPost
-from webpost import blog_list
+from blogpost import BlogPost
+from blogpost import blog_list
 
-#setting up the Environment for jinja
-#that sets jinja's relative directory to match the directory's name
-template_dir = path.join(path.dirname(__file__) , 'template')
-jinja_environment  = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(template_dir))
-
+#set up environment for Jinja
+#this sets jinja's relative directory to match the directory name(dirname) of
+#the current __file__, in this case, main.py
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 def constructBlogListHTML():
-    #blog_list = constructBlogList()
     html_string = "<ol>\n"
-    for i in range(0 , len(blog_list)):
+    for i in range(0, len(blog_list)):
         blog_post = blog_list[i]
-        html_string += "<li>" + str((blog_post.listString())) + "</li>"
-    html_string += '</ol>'
+        html_string +=  blog_post.listString(i) + "</li>"
+    html_string += "</ol>"
     return html_string
 
-class MainHandler(webapp2.RequestHandler):
+
+
+class RecipesHandler(webapp2.RequestHandler):
     def get(self):
-       joke_list = ['The only time incorrectly isn\'t spelled incorrectly is when it\'s spelled incorrectly.'
-        , 'Escalators don\'t break down... they just turn into stairs' ,
-        'I intend to live forever... or die trying' ,
-        'A blind man walks into a bar....And a table, and a chair' ,
-        'We never knew he was a drunk... until he showed up to work sober' ,
-        'A clear conscience is usually the sign of a bad memory' ,
-         'I used to be in a band, we were called \'lost dog\'. You probably saw our posters' ,
-         'I childproofed the house... but they still get in!' ,
-         'Want to hear a pizza joke? nah, it\'s too cheesy. What about a construction joke? Oh never mind, I\'m still working on that one. Did you hear the one about the rope? Skip it. Have you heard the one about the guy in the wheelchair? Never mind, it\'s too lame.'
-         , 'I used to think the brain was the most important organ. Then I thought, look what\'s telling me that.' ,
-          'Today a man knocked on my door and asked for a small donation towards the local swimming pool. I gave him a glass of water.'
-            ]
-        randnum = randint(0,(len(joke_list)-1))
+        recipes_list = ['templates/recipes/recipe1.html',
+                        'templates/recipes/recipe2.html',
+                        'templates/recipes/recipe3.html',
+                        'templates/recipes/recipe4.html',
+                        'templates/recipes/recipe5.html',
+                        'templates/recipes/recipe6.html'
+
+                       ]
+
+        randnum = randint(0,(len(recipes_list)-1))
+        recipe_file = open(recipes_list[randnum], 'r')
         #this is where you reference your HTML file
-        template = jinja_environment.get_template('templates/dummy.html')
-        my_vars = {'haha' : joke_list[randnum]}
+        template = jinja_environment.get_template('templates/index.html')
+        my_vars = {'ListOfRecipes' : recipe_file.read(),"bloglist": constructBlogListHTML()}
         self.response.out.write(template.render(my_vars))
+
+
 
 class PostHandler(webapp2.RequestHandler):
     def get(self):
         # This creates and serves the blog post page
-        page_id = self.request.get('page_id')
-        if not page_id:
-            page_id=0
-        else:
-            page_id= int(page_id)
-        blog_post = blog_list[page_id]
+        page_id = int(self.request.get('page_id'))
+        self.sendResponse(page_id, None)
 
-        template = jinja_environment.get_template(blog_post.template)
-        template_variables = {}
+    def post(self):
+        page_id = int(self.request.get('page_id'))
+        self.sendResponse(page_id, self.request.get("comment"))
+
+    def sendResponse(self, page_id, new_comment):
+        template = jinja_environment.get_template('templates/blogpost.html')
+        blog_post = blog_list[page_id]
+        usersName = self.request.get("usersName_submission")
+        commentHTML = blog_post.commentsAsHTML(page_id, new_comment,usersName)
+        usersName = self.request.get("usersName_submission")
+        template_variables = {"title": blog_post.title,
+                              "content": blog_post.content,
+                              "comments": commentHTML,
+                               }
         self.response.out.write(template.render(template_variables))
 
-
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/post',PostHandler)
+
+    ('/', RecipesHandler),
+    ('/post', PostHandler)
 ], debug=True)
